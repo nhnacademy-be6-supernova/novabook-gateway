58ecd26700543288b1b77c59c001be05d622be07
hyungwang <kfk10112@gmail.com>
2024-07-13 17:41:18 +0900

    feat: #18: 페이코 로그인 구현, 리팩토링

--- src/main/java/store/novabook/gateway/config/RedisConfig.java
+++ src/main/java/store/novabook/gateway/config/RedisConfig.java
@@ -1,6 +1,5 @@
 package store.novabook.gateway.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
